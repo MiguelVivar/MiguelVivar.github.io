bab01d69dd624904ccb09b85ef65472515689df1
MiguelVivar <miguelvivarfarfan@gmail.com>
2025-05-24 11:42:09 -0500

    feat: Remove unused blog page and add dynamic export to robots and sitemap

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  trailingSlash: true,
   images: {
     remotePatterns: [new URL('https://randomuser.me/api/portraits/**/**')],
   },
